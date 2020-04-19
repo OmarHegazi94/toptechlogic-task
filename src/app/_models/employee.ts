6043141974a10b302651c53d93bd2484243dd09b
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2020-04-19 11:22:33 +0200

    finished employee model

--- src/app/_models/employee.ts
+++ src/app/_models/employee.ts
@@ -1,2 +1,10 @@
 export class Employee {
+    constructor(
+        public ID: number,
+        public Name: string,
+        public Email: string,
+        public Password: string,
+        public Phone: number,
+        public Image: string
+    ){}
 }
